8282ca9c0f9320d7b39178d0039d504a6590f4ff
Ouafae MOUDNI <omoudni@e2r13p11.clusters.42paris.fr>
2022-03-30 21:32:05 +0200

    fixed the only 0s map

--- src/get_color.c
+++ src/get_color.c
@@ -6,7 +6,7 @@
 /*   By: omoudni <omoudni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 18:36:29 by omoudni           #+#    #+#             */
-/*   Updated: 2022/02/23 21:03:42 by omoudni          ###   ########.fr       */
+/*   Updated: 2022/03/30 21:31:30 by omoudni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ static float	get_grad(float z, int min, int max)
 	float	spectrum;
 	float	z_per;
 
+	if (min == max)
+		return ((float)min);
 	spectrum = max - min;
 	z_per = (z - min) / spectrum;
 	return (z_per);
